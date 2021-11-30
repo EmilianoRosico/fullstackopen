624880c4147df4fd79ffe7599dfdd4ebd8900e37
EmilianoRosico <emiliano19@gmail.com>
2021-11-30 20:11:32 -0300

    Excercise 1.7 complete.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,12 +7,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all,setAll] = useState(0)
 
-  const goodComment = () => setGood(good + 1)
-  const neutralComment = () => setNeutral(neutral + 1)
-  const badComment = () => setBad(bad + 1)
-  let average = isNaN((good-bad)/(good+neutral+bad)) ? 0:(good-bad)/(good+neutral+bad)
-  let percentage = isNaN(good/(good+neutral+bad)*100) ? 0:good/(good+neutral+bad)*100
+  const goodComment = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  const neutralComment = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const badComment = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+  let average = isNaN((good-bad)/all) ? 0:(good-bad)/all
+  let percentage = isNaN(good/all*100) ? 0:good/all*100
 
   return (
     <div>
@@ -33,7 +43,7 @@ const App = () => {
       <Display counter={good} text="Good"/>
       <Display counter={neutral} text="Neutral" />
       <Display counter={bad} text="Bad" />
-      <Display counter={good+neutral+bad} text="Total answers" />
+      <Display counter={all} text="Total answers" />
       <Display counter={average} text="Average" />
       <Display counter={percentage} text="Percentage" />
 
